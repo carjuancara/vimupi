75a8fad9b2165c0b99c9950651f86f8ed60125cf
Carlos Juan Carabajal <carjuancara@gmail.com>
2023-12-29 08:03:32 -0300

    feat: :card_file_box: cree la conexion con la DB

---
+++ api/src/db.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize')
+const  filesModel  = require('./Model/Files')
+const sequelize = new Sequelize(`postgres://postgres:postgres@localhost:5432/vimupiDB`, {
+    logging: false,
+    native:false
+})
+
+filesModel(sequelize)
+module.exports = {
+    ...sequelize.models,
+    sequelize
+}
+
