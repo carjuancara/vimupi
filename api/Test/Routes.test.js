a94c0fbf46a53da45a2db2c94852dd3c3bde0589
Carlos Juan Carabajal <carjuancara@gmail.com>
2024-01-04 17:37:42 -0300

    feat: :white_check_mark: cree los test para las rutas /upload /download

--- api/Test/Routes.test.js
+++ api/Test/Routes.test.js
@@ -1,37 +1,64 @@
 const request = require('supertest')
-const { conn } = require('../src/db')
+const { describe, expect, it } = require('@jest/globals')
+const fs = require('fs') // Necesitas importar el módulo 'fs' para usar fs.existsSync
+const path = require('path') // Necesitas importar el módulo 'path' para manejar rutas
 const app = require('../src/app')
-const { describe, expect, it, afterAll, beforeAll } = require('@jest/globals')
-// const fs = require('fs')
+// const fsExtra = require('fs-extra')
 
-describe('Rutas', () => {
-  beforeAll(async () => {
-    await conn.sync({ force: true })
-  })
-  it('Debe subir un archivo a la ruta "/upload" ', async () => {
+describe('Pruebas de subida de archivos', () => {
+  const performFileUploadTest = async (fileName, fileExtension) => {
     const response = await request(app)
-      .post('/uploads') // Ajusta la ruta según tu configuración
-      .attach('archivo', './src/uploads/image.jpg') // Ajusta la ruta del archivo que deseas subir
+      .post('/upload')
+      .attach('archivo', path.resolve(__dirname, `files/${fileName}.${fileExtension}`))
 
     expect(response.status).toBe(200)
     expect(response.body).toHaveProperty('message', 'Archivo recibido correctamente')
+
+    // Verificar que el archivo realmente existe en la carpeta de destino
+    const filePath = path.resolve(__dirname, `download/${fileName}.${fileExtension}`)
+    expect(fs.existsSync(filePath)).toBeTruthy()
+
+    // Limpiar el archivo después de la prueba
+    // fs.unlinkSync(filePath)
+  }
+
+  it('Debe subir el archivo "image" a la ruta "/upload"', async () => {
+    await performFileUploadTest('image', 'jpg')
   })
-  it('Devuelve un error si el archivo no es una imagen" ', async () => {
+
+  it('Debe subir el archivo "image2" a la ruta "/upload"', async () => {
+    await performFileUploadTest('image3', 'jpg')
+  })
+})
+
+describe('Rutas', () => {
+  it('El archivo index.js existe', () => {
+    const indexPath = path.resolve(__dirname, '../index.js')
+    expect(fs.existsSync(indexPath)).toBeTruthy()
+  })
+
+  it('Devuelve un error si el archivo no es una imagen', async () => {
     const response = await request(app)
-      .post('/uploads') // Ajusta la ruta según tu configuración
-      .attach('archivo', './src/uploads/readme.txt') // Ajusta la ruta del archivo que deseas subir
+      .post('/upload')
+      .attach('archivo', path.resolve(__dirname, 'files/readme.txt'))
 
     expect(response.status).toBe(400)
-    expect(response.body.error).toBe('debe ser un archivo de imagen válido')
+    expect(response.body.error).toBe('Tipo de archivo no permitido')
+
+    // Verificar que el archivo no se haya guardado en la carpeta de destino
+    const filePath = path.resolve(__dirname, 'download/readme.txt')
+    expect(fs.existsSync(filePath)).toBeFalsy()
   })
+
   it('Devuelve un error si el archivo es mayor a 100KB', async () => {
     const response = await request(app)
-      .post('/uploads')
-      .attach('archivo', './src/uploads/typescript.jpg')
+      .post('/upload')
+      .attach('archivo', path.resolve(__dirname, 'files/typescript.jpeg'))
 
     expect(response.status).toBe(400)
-    expect(response.body.error).toBe('la imagen no puede superar los 100KB')
+    expect(response.body.error).toBe('La imagen no puede superar los 100KB')
   })
+
   it('Devuelve todos los archivos /download', async () => {
     const response = await request(app).get('/download')
 
@@ -41,13 +68,9 @@ describe('Rutas', () => {
     // Verifica que cada elemento en el array sea un objeto
     response.body.forEach(file => {
       expect(typeof file).toBe('object')
+      expect(file).toHaveProperty('filename')
+      expect(file).toHaveProperty('size')
+      expect(file).toHaveProperty('content')
     })
-  })
-  /* it('El archivo index.js existe', () => {
-      const indexPath = 'index.js' // Asegúrate de ajustar la ruta según la ubicación de tu archivo index.js
-      expect(fs.existsSync(indexPath)).toBeTruthy()
-    }) */
-  afterAll(async () => {
-    conn.close()
   })
 })
